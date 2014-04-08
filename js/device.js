d1d5e4c24d779be54c385be564588a81cba938bb
Troy Daniel <troygdaniel@gmail.com>
2014-04-08 19:11:28 -0400

    Cleanup and revisions

--- js/device.js
+++ js/device.js
@@ -1,17 +1,17 @@
 var Almost = Almost || {};
 
 Almost.Device = function (options) {
-  var id, roomId, deviceName, socket, onUpdate
+  var roomId, socket, onUpdate
   var previousLocation =  {lat:-1, lng:-1};
   var currentLocation =   {lat:0, lng:0};
+  var id = Almost.Device.getDeviceId();
+  var deviceName = Almost.Device.fetchDeviceName();
 
   initialize(options);
 
   function initialize(opt) {
     // initialize with opt
     if (!opt) return;
-    if (opt.id)       { id = opt.id; }
-    if (opt.deviceName) { deviceName = opt.deviceName; }
     if (opt.roomId)   { roomId = opt.roomId; }
     if (opt.socket)   { socket = opt.socket; }
     if (opt.onUpdate) { onUpdate = opt.onUpdate; }
@@ -56,10 +56,42 @@ Almost.Device = function (options) {
   function getLng() {
     return currentLocation.lng;
   }
-
   return {
     toJSON: toJSON,
     getLat: getLat,
     getLng: getLng
   };
 };
+
+Almost.Device.getDeviceId = function () {
+  var deviceId;
+  
+  deviceId = localStorage.getItem("deviceId");
+  
+  if (!deviceId) {
+    deviceId = getGuid();
+    localStorage.setItem("deviceId", deviceId);
+  }
+  
+  return deviceId;
+}
+Almost.Device.fetchDeviceName = function() {
+  var d = localStorage.getItem("deviceName");
+  if (d === null || d === "undefined") d = "a friend";
+  return d;
+}
+Almost.Device.saveDeviceName = function(d) {
+  if (d === null || d === "undefined") d = "a friend";
+  localStorage.setItem("deviceName", d);    
+  return d;
+}
+
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+   .toString(16)
+   .substring(1);
+};
+
+function getGuid() {
+  return s4()+s4()+s4()+s4();
+}
--- js/room.js
+++ js/room.js
@@ -1,91 +1,57 @@
-//RoomPage
-var roomPage, device;
-
 // Page init
 $(function() {  
-  roomPage = new RoomPage();
-  var deviceId = getDeviceId()
-  var deviceName = getDeviceName();
-  var now = new Date();
-  document.title = "almost where | " + deviceName;
-  var deviceOptions = {
-    id: deviceId,
-    deviceName: deviceName,
-    roomId: roomPage.id,
-    socket: socket
-  };
-
-  device = new Almost.Device(deviceOptions);
-
-  // Receive updates from web sockets
-  socket.on('share_location_changes:' + roomPage.id, function(data) {
-
-      var dist = distanceFrom(device.getLat(), device.getLng(), data.lt, data.lg);
-       // var dist = distanceFrom(43.441559, -79.7486979, data.lt, data.lg);
-      var deviceName = data.n;
-      document.title = "almost where | " + deviceName;
-      $("#medium_text").html("from <br/>" + deviceName);
-      if (dist < 1) 
-        $("#large_text").text(parseInt(dist*1000)+"m");      
-      else 
-        $("#large_text").text(dist.toFixed(2)+" km");
-
-      if (dist > 500)
-        $("#large_text").text(":-(");
-
-    });
+  var roomPage = new RoomPage();
+  var device = new Almost.Device({ roomId: roomPage.id, socket: socket });
+  roomPage.listenForUpdates(device);
 });
 
-function getDeviceId() {
-  var deviceId;
-  deviceId = localStorage.getItem("deviceId");
-  if (!deviceId) {
-    deviceId = getGuid();
-    localStorage.setItem("deviceId", deviceId);
-  }
-  return deviceId;
-}
 
-function getDeviceName() {
-  var deviceName = localStorage.getItem("deviceName");
-  if (deviceName === null || deviceName === "deviceName") deviceName = "";
-  deviceName = prompt("Please name this device",deviceName);
-  localStorage.setItem("deviceName", deviceName);
-  return deviceName;
-}
 
 function RoomPage (options) {
   var id = location.search.substr(1, location.search.length);
-  return {
-    id: id
+
+  // Receive updates from web sockets
+  function listenForUpdates(device) {
+    
+    socket.on('share_location_changes:' + id, function(data) {
+        var dist = distanceFrom(device.getLat(), device.getLng(), data.lt, data.lg);
+        var deviceName = data.n;
+         // var dist = distanceFrom(43.441559, -79.7486979, data.lt, data.lg);
+
+        document.title = "almost where | " + deviceName;
+        
+        $("#medium_text").html("from <br/>" + deviceName);
+        
+        if (dist < 1) 
+          $("#large_text").text(parseInt(dist*1000)+"m");      
+        else 
+          $("#large_text").text(dist.toFixed(2)+" km");
+
+        if (dist > 500)
+          $("#large_text").text(":-(");
+
+      });
   }
-}
-
-  function s4() {
-      return Math.floor((1 + Math.random()) * 0x10000)
-           .toString(16)
-           .substring(1);
-  };
-
-  function getGuid() {
-      return s4()+s4()+s4()+s4();
-  }
-
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
-}
-
-function distanceFrom(lat1,lon1,lat2,lon2) {
   
-var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
-  return d;
+  // Calculate the distance between two coordinates
+  function distanceFrom(lat1,lon1,lat2,lon2) {    
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a =  Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+  }  
+  return {
+    id: id,
+    listenForUpdates: listenForUpdates
+  }
+
+  // Math Rad function
+  function deg2rad(deg) {
+    return deg * (Math.PI/180)
+  } 
+
 }
+
